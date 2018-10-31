8248d42545e54ac9976f3b95238b81d5b2c388d2
Scott Ian <sim2487@gmail.com>
2018-10-31 09:32:11 -0700

    self-reference drill

--- object-drills1.js
+++ object-drills1.js
@@ -1,44 +1,60 @@
 'use strict';
 
 // https://courses.thinkful.com/web-dev-001v1/project/2.6.2
-
-/* **********************************************
-Object creator
-********************************************** */
-const creatMyObject = () =>{
-  return {
-  foo: 'bar',
-  answerToUniverse: 42,
-  "olly olly": 'oxen free',
-  sayHello: () => "hello",
- };
-};
-console.log(creatMyObject());
-
-
-/* **********************************************
-Object updater
-********************************************** */
-
-const myObj = {
-  item1: 'Fruit',
-  item2: 'Cantaloupe'
-};
-
-function updateObject(obj){
-  obj.foo = 'foo';
-  obj.bar = 'bar';
-  obj.bizz = 'bizz';
-  obj.bang = 'bang';
-  return obj;
-}
-
-console.log(updateObject(myObj));
+//
+// /* **********************************************
+// Object creator
+// ********************************************** */
+// const creatMyObject = () =>{
+//   return {
+//   foo: 'bar',
+//   answerToUniverse: 42,
+//   "olly olly": 'oxen free',
+//   sayHello: () => "hello",
+//  };
+// };
+// console.log(creatMyObject());
+//
+//
+// /* **********************************************
+// Object updater
+// ********************************************** */
+//
+// const myObj = {
+//   item1: 'Fruit',
+//   item2: 'Cantaloupe'
+// };
+//
+// function updateObject(obj){
+//   obj.foo = 'foo';
+//   obj.bar = 'bar';
+//   obj.bizz = 'bizz';
+//   obj.bang = 'bang';
+//   return obj;
+// }
+//
+// console.log(updateObject(myObj));
 
 
 /* **********************************************
 Self-reference
 ********************************************** */
+function personMaker(firstName, lastName) {
+  const person = {
+    firstName,
+    lastName,
+    // replace `null` with a function that uses self reference to return
+    // full name
+    fullName: function() {
+      return `${this.firstName} ${this.lastName}`;
+    }
+  };
+
+  return person;
+}
+let person = personMaker("Paul", "Jones");
+console.log(person.fullName());
+
 
 
 
